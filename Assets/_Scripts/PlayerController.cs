558d080751654b8d93b9f955ad68cd2c8ed60efb
jeffreyruffolo <jarpqd@mail.missouri.edu>
2016-04-16 01:38:50 -0500

    arena
    ^^

---
+++ Assets/_Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hmove = Input.GetAxis("Horizontal");
+        float vmove = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(hmove, 0f, vmove);
+
+        rb.velocity = movement * 20;
+    }
+}
