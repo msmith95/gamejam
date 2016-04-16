96badc363e726a8195c457a1f72c6d010a3a05b4
Michael Smith Jr <mdsf3f@mail.missouri.edu>
2016-04-16 10:50:02 -0500

    Added some scripts

---
+++ Assets/_Scripts/PlayerController 2.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+	public float jumpSpeed;
+
+	private Rigidbody rb;
+	private int count;
+	public Text countText;
+	public Text winText;
+	public GameObject punchPrefab;
+
+	public float fireRate;
+	private float nextFire;
+
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void Update(){
+		
+	}
+
+
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.velocity = movement * 10;
+		//rb.AddForce (movement * speed);
+
+		if (Input.GetKeyDown ("space") && Time.time > nextFire) {
+			GameObject punch = Instantiate(punchPrefab, transform.position + new Vector3 (0.0f, 0.0f, 0.5f), new Quaternion(0f, transform.rotation.y, 0f, 0f)) as GameObject;
+				punch.tag = "bullet";
+			    nextFire = Time.time + fireRate;
+				//punch.GetComponent<Rigidbody>().AddForce(transform.forward * 50000);
+		}
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			count = count + 1;
+			SetCountText ();
+		}
+	}
+
+	void SetCountText(){
+		countText.text = "Count: " + count.ToString ();
+		if (count >= 12)
+		{
+			winText.text = "You Win!";
+		}
+	}
+}
---
+++ Assets/_Scripts/fire.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class fire : MonoBehaviour {
+	Rigidbody rb;
+	// Use this for initialization
+	float xVel, zVel;
+
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+
+		xVel = Input.GetAxis ("Horizontal");
+		zVel = Input.GetAxis ("Vertical");
+
+		print (xVel);
+		if (Mathf.Abs (xVel) < .8f && xVel != 0f) {
+			if(xVel < 0f) xVel = -.8f;
+			else xVel = .8f;
+		}
+		if (Mathf.Abs (zVel) < .8f  && zVel != 0f) {
+			if(zVel < 0f) zVel = -.8f;
+			else zVel = .8f;
+		}
+		if (xVel == 0f && zVel == 0f) {
+			Destroy (gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 movement = new Vector3(xVel, 0f, zVel);
+		rb.velocity = movement * 10;
+	}
+}
