f8285e70f4697f3ed2dc6e22e5782797eb705926
jeffreyruffolo <jarpqd@mail.missouri.edu>
2016-04-17 00:29:51 -0500

    player health!

--- Assets/_Scripts/PlayerHealthController.cs
+++ Assets/_Scripts/PlayerHealthController.cs
@@ -4,17 +4,20 @@ using UnityEngine.UI;
 
 public class PlayerHealthController : MonoBehaviour {
 
-    public PlayerHealth ph;
+    PlayerHealth ph;
 
     public Text healthText1, healthText2;
-
     public int totalHealth;
 
     // Use this for initialization
     void Start()
     {
-        healthText1 = GameObject.Find("PH1").GetComponent<Text>() as Text;
-        healthText2 = GameObject.Find("PH2").GetComponent<Text>() as Text;
+        ph = GameObject.FindGameObjectWithTag("Scripts").GetComponent<PlayerHealth>();
+        print(ph.getPH1());
+
+        healthText1 = GameObject.FindGameObjectWithTag("PH1").GetComponent<Text>() as Text;
+        healthText2 = GameObject.FindGameObjectWithTag("PH2").GetComponent<Text>() as Text;
+        
     }
 
     // Update is called once per frame
@@ -22,8 +25,6 @@ public class PlayerHealthController : MonoBehaviour {
     {
         if (gameObject.CompareTag("greenTeam")) totalHealth = ph.getPH1();
         if (gameObject.CompareTag("blueTeam")) totalHealth = ph.getPH2();
-
-        //healthText.text = "HP - " + totalHealth.ToString();
 
         if (gameObject.CompareTag("greenTeam")) healthText1.text = ph.getPH1().ToString();
         if (gameObject.CompareTag("blueTeam")) healthText2.text = ph.getPH2().ToString();
@@ -33,15 +34,31 @@ public class PlayerHealthController : MonoBehaviour {
     {
         if (other.gameObject.CompareTag("balloon"))
         {
-            if (gameObject.CompareTag("greenTeam") && other.GetComponent<fire>().getTeam() == 1) ph.decPH1(10);
-            if (gameObject.CompareTag("blueTeam") && other.GetComponent<fire>().getTeam() == 0) ph.decPH2(10);
-            Destroy(other.gameObject);
+            if (gameObject.CompareTag("greenTeam") && other.GetComponent<fire>().getTeam() == 1)
+            {
+                ph.decPH1(10);
+                Destroy(other.gameObject);
+
+            }
+            if (gameObject.CompareTag("blueTeam") && other.GetComponent<fire>().getTeam() == 0)
+            {
+                ph.decPH2(10);
+                Destroy(other.gameObject);
+            }
+            
         }
         else if (other.gameObject.CompareTag("squirt"))
         {
-            if (gameObject.CompareTag("greenTeam") && other.GetComponent<fire>().getTeam() == 1) ph.decPH1(1);
-            if (gameObject.CompareTag("blueTeam") && other.GetComponent<fire>().getTeam() == 0) ph.decPH2(1);
-            Destroy(other.gameObject);
+            if (gameObject.CompareTag("greenTeam") && other.GetComponent<fire>().getTeam() == 1)
+            {
+                ph.decPH1(1);
+                Destroy(other.gameObject);
+            }
+            if (gameObject.CompareTag("blueTeam") && other.GetComponent<fire>().getTeam() == 0)
+            {
+                ph.decPH2(1);
+                Destroy(other.gameObject);
+            }
         }
     }
 }
