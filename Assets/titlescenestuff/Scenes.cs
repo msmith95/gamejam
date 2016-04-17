9ab28ef03bf66858a2eb0977dba1621fa9442215
john-carmichael <jdckw4@mail.missouri.edu>
2016-04-16 23:49:06 -0500

    added stuff for title screen and drop

---
+++ Assets/titlescenestuff/Scenes.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Scenes : MonoBehaviour {
+
+	public void LoadScene(){
+		SceneManager.LoadScene (1);
+	}
+}
