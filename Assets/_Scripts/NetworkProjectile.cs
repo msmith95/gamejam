02d9ccc4874658646bcb05a8492c33e7624c8a50
Michael Smith Jr <mdsf3f@mail.missouri.edu>
2016-04-16 21:44:35 -0500

    Added Network Projectile

--- Assets/_Scripts/NetworkProjectile.cs
+++ Assets/_Scripts/NetworkProjectile.cs
@@ -1,22 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NetworkCharacter : Photon.MonoBehaviour {
+public class NetworkProjectile : Photon.MonoBehaviour {
 
 	private Vector3 correctPlayerPos;
 	private Quaternion correctPlayerRot;
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (!photonView.isMine)
 		{
-			transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
-			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+			//transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+			//transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+			transform.position = correctPlayerPos;
+			transform.position = correctPlayerRot;
 		}
 	}
 
