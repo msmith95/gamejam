831d309f413675ef315ab8ce28d34539bfaf5c90
Michael Smith Jr <mdsf3f@mail.missouri.edu>
2016-04-16 19:29:29 -0500

    Added basic networking capabilities

---
+++ Assets/_Scripts/GameLogic.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameLogic : MonoBehaviour {
+
+	private static PhotonView ScenePhotonView;
+
+	// Use this for initialization
+	void Start () {
+		ScenePhotonView = this.GetComponent<PhotonView>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public static int playerWhoIsIt;
+
+	void OnJoinedRoom()
+	{
+		// game logic: if this is the only player, we're "it"
+		if (PhotonNetwork.playerList.Length == 1)
+		{
+			playerWhoIsIt = PhotonNetwork.player.ID;
+		}
+
+		Debug.Log("playerWhoIsIt: " + playerWhoIsIt);
+	}
+
+	[PunRPC]
+	void TaggedPlayer(int playerID)
+	{
+		playerWhoIsIt = playerID;
+		Debug.Log("TaggedPlayer: " + playerID);
+	}
+
+	void OnPhotonPlayerConnected(PhotonPlayer player)
+	{
+		Debug.Log("OnPhotonPlayerConnected: " + player);
+
+		// when new players join, we send "who's it" to let them know
+		// only one player will do this: the "master"
+
+		if (PhotonNetwork.isMasterClient)
+		{
+			TagPlayer(playerWhoIsIt);
+		}
+	}
+
+	public static void TagPlayer(int playerID)
+	{
+		Debug.Log("TagPlayer: " + playerID);
+		ScenePhotonView.RPC("TaggedPlayer", PhotonTargets.All, playerID);
+	}
+
+	void OnPhotonPlayerDisconnected(PhotonPlayer player)
+	{
+		Debug.Log("OnPhotonPlayerDisconnected: " + player);
+
+		if (PhotonNetwork.isMasterClient)
+		{
+			if (player.ID == playerWhoIsIt)
+			{
+				// if the player who left was "it", the "master" is the new "it"
+				TagPlayer(PhotonNetwork.player.ID);
+			}
+		}
+	}
+}
---
+++ Assets/_Scripts/Randomizer.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using Photon;
+public class Randomizer : Photon.PunBehaviour {
+
+	GameObject monster;
+	// Use this for initialization
+	void Start () {
+		//PhotonNetwork.logLevel = PhotonLogLevel.Full;
+		PhotonNetwork.ConnectUsingSettings ("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void OnJoinedLobby()
+	{
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	void OnGUI(){
+		GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+	}
+
+	void OnPhotonRandomJoinFailed()
+	{
+		Debug.Log("Can't join random room!");
+		PhotonNetwork.CreateRoom(null);
+	}
+	public override void OnJoinedRoom ()
+	{
+		if (PhotonNetwork.playerList.Length == 1) {
+			monster = PhotonNetwork.Instantiate ("Player", new Vector3 (-57.6f, 4f, 0f), Quaternion.identity, 0);
+		} else {
+			monster = PhotonNetwork.Instantiate ("Player", new Vector3 (57.6f, 4f, 0f), Quaternion.identity, 0);
+		}
+		PlayerController controller = monster.GetComponent<PlayerController> ();
+		controller.enabled = true;
+	}
+}
