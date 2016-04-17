838277485073a3905afe24fab4b8acf179236767
Michael Smith Jr <mdsf3f@mail.missouri.edu>
2016-04-16 21:01:41 -0500

    Created Network Sprite Plane script

---
+++ Assets/_Scripts/NetworkSpritePlane.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkSpritePlane : Photon.MonoBehaviour {
+
+	private Quaternion correctPlayerRot;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (!photonView.isMine)
+		{
+			transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+		}
+	}
+
+	public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+	{
+		if (stream.isWriting)
+		{
+			// We own this player: send the others our data
+			stream.SendNext(transform.rotation);
+		}
+		else
+		{
+			// Network player, receive data
+			this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+		}
+	}
+}
